1e3e9a7ace6a1fb6bc5ecd2a76c7d04f13dc3844
IgorNiNo <vis_74@mail.ru>
2021-01-29 20:01:30 +0300

    lesson6 task5

--- app/src/main/java/com/example/android_1_05012021/NotesFragment.java
+++ app/src/main/java/com/example/android_1_05012021/NotesFragment.java
@@ -1,11 +1,14 @@
 package com.example.android_1_05012021;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +22,7 @@ import java.util.Calendar;
 
 public class NotesFragment extends Fragment {
 
+    private boolean isLandscape;
     protected static Notes[] notesView;
 
     // При создании фрагмента укажем его макет
@@ -56,10 +60,42 @@ public class NotesFragment extends Fragment {
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    showPortListOfNote(fi);
+                    showListOfNote(fi);
                 }
             });
         }
+    }
+
+    // activity создана, можно к ней обращаться. Выполним начальные действия
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Определение, можно ли будет расположить рядом заметку в другом фрагменте
+        isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        // Если можно нарисовать рядом заметку, то сделаем это
+        if (isLandscape) {
+            showLandListOfNote(0);
+        }
+    }
+
+    private void showListOfNote(int index) {
+        if (isLandscape) {
+            showLandListOfNote(index);
+        } else {
+            showPortListOfNote(index);
+        }
+    }
+
+    // Показать заметку в ландшафтной ориентации
+    private void showLandListOfNote(int index) {
+        // Создаём новый фрагмент с текущей позицией для вывода заметки
+        ListOfNoteFragment detail = ListOfNoteFragment.newInstance(index);
+        // Выполняем транзакцию по замене фрагмента
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_list_of_note, detail);  // замена фрагмента
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
     }
 
     // Показать заметку в портретной ориентации.

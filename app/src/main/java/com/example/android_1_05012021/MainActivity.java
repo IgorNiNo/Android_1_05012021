65990270f0e195e69a320b12e2f9e3e46036f5d5
IgorNiNo <vis_74@mail.ru>
2021-01-30 23:58:05 +0300

    lesson7 task1_1

--- app/src/main/java/com/example/android_1_05012021/MainActivity.java
+++ app/src/main/java/com/example/android_1_05012021/MainActivity.java
@@ -3,22 +3,84 @@ package com.example.android_1_05012021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-     @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         if (savedInstanceState == null) {
-             // Если эта activity запускается первый раз (с каждой новой заметкой первый раз),
-             // то перенаправим параметр фрагменту
-             NotesFragment details = new NotesFragment();
-             details.setArguments(getIntent().getExtras());
-             // Добавим фрагмент на activity
-             getSupportFragmentManager()
-                     .beginTransaction()
-                     .replace(R.id.fragment_container, details).commit();
-         }
-     }
+        initToolbar();
+        if (savedInstanceState == null) {
+            // Если эта activity запускается первый раз (с каждой новой заметкой первый раз),
+            // то перенаправим параметр фрагменту
+            NotesFragment details = new NotesFragment();
+            details.setArguments(getIntent().getExtras());
+            // Добавим фрагмент на activity
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, details).commit();
+        }
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пункта меню приложения (активити)
+        int id = item.getItemId();
+        if (navigateFragment(id)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean navigateFragment(int id) {
+//        switch (id) {
+//            case R.id.action_settings:
+//                addFragment(new SettingsFragment());
+//                return true;
+//            case R.id.action_main:
+//                addFragment(new MainFragment());
+//                return true;
+//            case R.id.action_favorite:
+//                addFragment(new FavoriteFragment());
+//                return true;
+//        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search); // поиск пункта меню поиска
+        SearchView searchText = (SearchView) search.getActionView(); // строка поиска
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // реагирует на конец ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            // реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+
 }
